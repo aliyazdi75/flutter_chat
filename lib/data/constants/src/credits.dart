23d5a05bb675594d118d983f28b1dd5ea42f24c6
aliyazdi75 <ma.yazdi75@gmail.com>
2021-03-04 22:37:48 +0330

    add log out and un authorized request triggers log out event and navigate to splash and login screen

--- lib/data/constants/src/credits.dart
+++ lib/data/constants/src/credits.dart
@@ -1,4 +1,4 @@
 const testEmail1 = 'ma.yazdi75@gmail.com';
-const testPassword1 = '@Abc1234';
+const testPassword1 = '';
 const testEmail2 = 'test_kKsfrZfM@gmail.com';
-const testPassword2 = '@Abc123';
+const testPassword2 = '';
