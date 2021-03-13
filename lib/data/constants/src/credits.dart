84340002d655f97aca1ef5e11c3abe04ebec07af
aliyazdi75 <ma.yazdi75@gmail.com>
2021-03-13 17:23:32 +0330

    add enable disable call

--- lib/data/constants/src/credits.dart
+++ lib/data/constants/src/credits.dart
@@ -1,4 +1,4 @@
 const testEmail1 = 'ma.yazdi75@gmail.com';
-const testPassword1 = '@Abc123';
+const testPassword1 = '';
 const testEmail2 = 'test_USpkcdSe@gmail.com';
-const testPassword2 = '@Abc123';
+const testPassword2 = '';
--- lib/data/constants/src/server.dart
+++ lib/data/constants/src/server.dart
@@ -1,7 +1,7 @@
 const apiVersion = '1';
-const apiKeyword = 'mateapi';
-const apiVersionHeader = 'X-MATEAPI-VERSION';
-const authHeader = 'X-MATEAPI-AUTH';
-const signalRAuthQuery = 'X-MATEAPI-AUTH';
+const apiKeyword = 'pi';
+const apiVersionHeader = 'API-VERSION';
+const authHeader = 'API-AUTH';
+const signalRAuthQuery = 'API-AUTH';
 const contentTypeKeyHeader = 'content-type';
 const contentTypeValueHeader = 'application/json; charset=utf-8';
